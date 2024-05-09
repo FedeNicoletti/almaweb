b9c0806450e6ef839b4aaad9eb7fb53faa5a4e73
Federico Nicoletti <63601533+FedeNicoletti@users.noreply.github.com>
2024-05-09 05:33:14 -0300

    Update Carousel.jsx

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -9,7 +9,7 @@ const Carousel = ({ children: sliderImages }) => {
   const next = () =>
     setCurr((curr) => (curr === sliderImages.length - 1 ? 0 : curr + 1));
   return (
-    <div className="overflow-hidden relative">
+    <div className="w-screen overflow-hidden relative">
       <div
         className="flex transition-transform ease-out duration-500"
         style={{ transform: `translateX(-${curr * 100}%)` }}
